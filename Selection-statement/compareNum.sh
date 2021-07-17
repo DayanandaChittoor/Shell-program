633529429db89ba658c686557197e746e8a5b57d
DayanandaChittoor <seenu.daya5@gmail.com>
2021-07-17 17:57:33 +0530

    Added Sequence-statement and Selection-statement

---
+++ Selection-statement/compareNum.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+read -p "Enter first number : " x
+read -p "Enter Second number : " y
+
+if [ $x -gt $y ]
+then 
+	echo "$x is greater than $y"
+elif [ $x -eq $y ]
+then
+	echo "$x is equal to $y"
+else
+	echo "$y is greater than $x"
+fi
+
